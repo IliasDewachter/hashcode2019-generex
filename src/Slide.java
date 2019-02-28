41e6f8831f3389c18d55320086098297267b0264
Ilias Dewachter <ilias.dewachter@student.kdg.be>
2019-02-28 20:04:09 +0100

    xx

--- src/Slide.java
+++ src/Slide.java
@@ -2,37 +2,20 @@ import java.util.Arrays;
 
 public class Slide {
 
-    private String id;
-    private int[] tags;
+    public String id;
+    public int[] tags;
 
     public Slide(String id, int[] tags) {
         this.id = id;
         this.tags = tags;
     }
 
-    public int[] getTags() {
-        return tags;
-    }
-
-    public void setTags(int[] tags) {
-        this.tags = tags;
-    }
-
-
     @Override
     public String toString() {
         return "Slide{" +
-                "id=" + id +
+                "id='" + id + '\'' +
                 ", tags=" + Arrays.toString(tags) +
                 '}';
-    }
-
-    @Override
-    public String toString() {
-        if (photo2 == null)
-            return String.valueOf(getPhoto1().getId());
-
-        return String.valueOf(getPhoto1().getId()) + " " + String.valueOf(getPhoto2().getId());
     }
 }
 
