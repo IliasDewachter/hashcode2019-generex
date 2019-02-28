91fa83758d7af7ae4ef14f5b350997a7be5c9b51
Ilias Dewachter <ilias.dewachter@student.kdg.be>
2019-02-28 20:57:37 +0100

    xxxx

--- src/Main.java
+++ src/Main.java
@@ -45,7 +45,89 @@ public class Main {
             data.slides.add(new Slide(firstSlide.id + " " + lastSlide.id, tagz));
         }
 
+        Collections.shuffle(data.slides);
+
+        int highestCommon = 0;
+        Slide highestSlide1 = null;
+        Slide highestSlide2 = null;
+        for (Slide slide1 : data.slides) {
+            for (Slide slide2 : data.slides) {
+                int common = calculateCommon(slide1, slide2);
+                if (common > highestCommon) {
+                    highestCommon = common;
+                    highestSlide1 = slide1;
+                    highestSlide2 = slide2;
+                }
+            }
+        }
+        data.slides.remove(highestSlide1);
+        data.slides.remove(highestSlide2);
+
+        LinkedList<Slide> result = new LinkedList<>();
+        result.push(highestSlide1);
+        result.push(highestSlide2);
+
+        while (data.slides.size() > 0) {
+            System.out.println("adding");
+
+            int highestScore = 0;
+            Slide highestSlide = null;
+            boolean isStart = false;
+            for (Slide slide : data.slides) {
+
+                //check start of snake
+                {
+                    Slide slide2 = result.get(0);
+                    Slide slide3 = result.get(1);
+                    int score = calculateScore(slide, slide2, slide3);
+                    if (score > highestScore) {
+                        highestScore = score;
+                        highestSlide = slide;
+                        isStart = true;
+                    }
+                }
+
+                {
+                    Slide slide1 = result.get(result.size() - 2);
+                    Slide slide2 = result.getLast();
+                    int score = calculateScore(slide1, slide2, slide);
+                    if (score > highestScore) {
+                        highestScore = score;
+                        highestSlide = slide;
+                        isStart = false;
+                    }
+                }
+
+            }
+
+            if (isStart) {
+                result.offerFirst(highestSlide);
+            } else {
+                result.offerLast(highestSlide);
+            }
+
+            data.slides.remove(highestSlide);
+
+        }
 
 
+        OutputReader.writeFile(result);
+    }
+
+
+    public static int calculateCommon(Slide slide1, Slide slide2) {
+        int common = 0;
+        for (int tag : slide1.tags) {
+            for (int innerTag : slide2.tags) {
+                if (tag == innerTag) common++;
+            }
+        }
+        return common;
+    }
+
+    public static int calculateScore(Slide slide1, Slide slide2, Slide slide3) {
+        int common1 = calculateCommon(slide1, slide2);
+        int common2 = calculateCommon(slide2, slide3);
+        return Math.min(common1, common2);
     }
 }
--- src/OutputReader.java
+++ src/OutputReader.java
@@ -2,15 +2,16 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 
 public class OutputReader {
-    public static void writeFile(Slide[] slides) {
+    public static void writeFile(List<Slide> slides) {
         String filename = "output/output.txt";
         try(BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
-            writer.append(String.valueOf(slides.length));
+            writer.append(String.valueOf(slides.size()));
             for (Slide slide : slides) {
                 writer.newLine();
-                writer.append(slide.toString());
+                writer.append(slide.id);
             }
         } catch (IOException e) {
             System.err.println(e.getMessage());
